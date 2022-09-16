32975ea72d2219819aea1c2822e21f6f17e1cd2d
Owen Cooper <ogooper77@gmail.com>
2022-09-16 12:20:39 -0500

    Add Java source

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+   public static void main(String[] a) {
+     
+      Marker m1 = new Marker();
+      Marker m2 = new Marker(true);
+      Marker m3 = new Marker(true, "black", "Expo");
+
+
+     
+  }
+}
---
+++ Marker.java
@@ -0,0 +1,47 @@
+public class Marker {
+  private boolean capStatus;
+  private String markerColor;
+  private String markerBrand;
+
+  public Marker() {
+    capStatus = true;
+    markerColor = "Black";
+    markerBrand = "Expo";
+  }
+  public Marker(boolean capStatus) {
+    this();
+    this.capStatus = capStatus;
+  }
+  public Marker(boolean capStatus, String color, String brand) {
+    this.capStatus = capStatus;
+    markerColor = color;
+    markerBrand = brand;
+    
+  }
+
+  @Override
+  public String toString() {
+    return "The cap is: " + capStatus + " The marker color is: " + markerColor + " The marker brand is: " + markerBrand;
+  }
+
+  public boolean tellCap() {
+   return capStatus;
+  }
+  public String tellColor() {
+    return markerColor;
+  }
+
+  public String tellBrand() {
+    return markerBrand;
+  }
+
+  public void setColor(String color) {
+    color = markerColor;
+  }
+  public void setBrand(String brand) {
+    brand = markerBrand;
+  }
+  public void setCap(boolean capStatus) {
+    this.capStatus = capStatus;
+  }
+}
