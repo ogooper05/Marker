ec68ee16461946d7ed3163def6555c31b6192f68
Owen Cooper <ogooper77@gmail.com>
2022-09-21 11:48:52 -0500

    Marker with purell

--- Main.java
+++ Main.java
@@ -4,11 +4,15 @@ public class Main {
       Marker m1 = new Marker();
       Marker m2 = new Marker(true);
       Marker m3 = new Marker(true, "black", "Expo");
+      Sanatizer s1 = new Sanatizer(); 
+      Marker m4 = new Marker(s1);
 
 
      System.out.println(m1);
      System.out.println(m2);
      System.out.println(m3);
+     m4.sanitaryMarker();
+
 
   }
 }
